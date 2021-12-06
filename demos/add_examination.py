0ae2f0870f45f48829c8e6034331f9e090ea9d79
Gustav Henriks <gustav.henriks@mikajaki.com>
2021-12-06 14:06:10 +0000

    Revert "Update speech because of conflict wth headstarting REVO"
    This reverts commit 024b339f811d67084b0652936771aa68cdb1763e.

--- demos/add_examination.py
+++ demos/add_examination.py
@@ -11,19 +11,21 @@ import requests
 
 station_ip = 'localhost' # or 'localhost' or '10.1.1.1'
 
-patient_id = 4
-examination_id = 2
+patient_id = 8
+examination_id = 8
 first_name = 'TEST'
 last_name = 'ABCDE'
 chin_z = 1522
 chin_to_eyeline = 102
-instruments = ['REVO', 'VX120'] # 'REVO' and/or 'VX120'
+instruments = ['VX120', 'REVO'] # 'REVO' and/or 'VX120'. For both, VX120 needs to be first.
 gender = 'Female' #'Female' or 'Male'
 birth_date = '1959-02-15'
 
 
 patient = {
         'patient_name':first_name+' '+last_name,
+        'first_name':first_name,
+        'last_name':last_name,
         'patient_id':patient_id,
         'examination_id':examination_id,
         'morphology':{'chin_z': chin_z, 'chin_to_eyeline': chin_to_eyeline},
